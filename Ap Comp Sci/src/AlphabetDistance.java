676e5f500c9ae17b4dc9bd6de849815c5512a03f
529565 <529565@8RM4-01.cuhsd-int.prv>
2015-08-28 09:56:40 -0700

    Work on alphabetdistance

---
+++ Ap Comp Sci/src/AlphabetDistance.java
@@ -0,0 +1,17 @@
+
+public class AlphabetDistance
+{
+
+	public static void main(String[] args)
+	{
+		String s = "hello";
+		System.out.println((int)s.charAt(0));
+	}
+	
+	public static int GetNum(String str)
+	{
+		int n = 0;
+		return n;
+	}
+
+}
