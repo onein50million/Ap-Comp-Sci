141c07ea7449eab7b49823a21e7b23dcf0042f2a
529565 <529565@8RM4-01.cuhsd-int.prv>
2015-09-01 11:32:33 -0700

    Docs, yo

--- Ap Comp Sci/chapter3/Student.java
+++ Ap Comp Sci/chapter3/Student.java
@@ -1,4 +1,8 @@
-
+/**
+ * 
+ * Keeps track of student scores
+ *
+ */
 public class Student
 {
 	String name;
@@ -6,27 +10,47 @@ public class Student
 	double average;
 	int quizN;
 	
-	public Student(String str)
+	/**
+	 * Creates a new student
+	 * @param name Student's name
+	 */
+	public Student(String name)
 	{
-		this.name = str;
+		this.name = name;
 		this.quizN = 0;
 	}
 	
+	/**
+	 * Returns the student's name
+	 * @return the student's name
+	 */
 	public String getName()
 	{
 		return this.name;
 	}
 	
+	/**
+	 * Returns the student's total score
+	 * @return the sum of all test scores
+	 */
 	public int getTotalScore()
 	{
 		return this.score;
 	}
 	
+	/**
+	 * Return's the student's average score
+	 * @return the average score over all tests
+	 */
 	public double getAverageScore()
 	{
 		return this.average;
 	}
 	
+	/**
+	 * Add's a quiz to the student's score
+	 * @param score the score the student received on the test
+	 */
 	public void addQuiz(int score)
 	{
 		this.quizN += 1;
