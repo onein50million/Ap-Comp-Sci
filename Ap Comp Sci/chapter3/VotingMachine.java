44308f4189eb49b1d1e12d6bd2f7fc12c672044e
529565 <529565@8RM4-01.cuhsd-int.prv>
2015-09-01 11:42:00 -0700

    Added docs for voting machine

--- Ap Comp Sci/chapter3/VotingMachine.java
+++ Ap Comp Sci/chapter3/VotingMachine.java
@@ -1,30 +1,53 @@
-
+/**
+ * 
+ * Virtual voting machine
+ *
+ */
 public class VotingMachine
 {
 	int Dvotes;
 	int Rvotes;
 	
+	/**
+	 * Add's votes to the democratic party
+	 * @param n number of votes to add
+	 */
 	public void addDvote(int n)
 	{
 		this.Dvotes += n;
 	}
 	
+	/**
+	 * Add's votes to the republican party
+	 * @param n number of votes to add
+	 */
 	public void addRvote(int n)
 	{
 		this.Rvotes += n;
 	}
 	
+	/**
+	 * Clears votes for all parties
+	 */
 	public void Clear()
 	{
 		this.Dvotes = 0;
 		this.Rvotes = 0;
 	}
 	
+	/**
+	 * Returns the number of votes for the democratic party
+	 * @return votes for the democratic party
+	 */
 	public int getDvotes()
 	{
 		return this.Dvotes;
 	}
 	
+	/**
+	 * Returns the number of votes for the democratic party
+	 * @return votes for the democratic party
+	 */
 	public int getRvotes()
 	{
 		return this.Rvotes;
