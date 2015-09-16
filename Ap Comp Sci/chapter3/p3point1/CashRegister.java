44a899b10cef406b6fe02473768b7e891aa59b7d
529565 <529565@8RM4-01.cuhsd-int.prv>
2015-09-16 11:22:38 -0700

    Did CashRegister

---
+++ Ap Comp Sci/chapter3/p3point1/CashRegister.java
@@ -0,0 +1,94 @@
+package p3point1;
+
+public class CashRegister
+{
+	private int cents;
+	private int payedCents;
+	private int itemCount;
+	private double tax;
+
+	/**
+	 * Generates a cash register with specific tax rate
+	 * @param tax the tax rate in decimal
+	 */
+	public CashRegister(double tax)
+	{
+		this.tax = tax + 1;
+	}
+	
+	private int Convert(double amount)
+	{
+		int tempDollars = (int) Math.floor(amount);
+		int tempCents = (int) Math.round((amount - tempDollars) * 100);
+		return (tempDollars * 100) + tempCents;
+	}
+	
+	/**
+	 * Resets all values for the next client
+	 * @return the amount the client has payed so far
+	 */
+	public double Reset()
+	{
+		double tempCents = this.cents;
+		this.payedCents = 0;
+		this.cents = 0;
+		this.itemCount = 0;
+		return tempCents / 100;
+	}
+	
+	/**
+	 * Adds an purchase to the total of purchases
+	 * @param amount amount in dollars of the purchased item
+	 * @param tax whether or not to tax the item
+	 */
+	public void PurchaseItem(double amount, boolean tax)
+	{
+		int cents = Convert(amount);
+		if (tax)
+		{
+			this.cents += (int) Math.round(cents * this.tax);
+		}
+		else
+		{
+			this.cents += cents;
+		}
+		this.itemCount += 1;
+	}
+	
+	/**
+	 * Pays the register with a specific amount of money
+	 * @param amount amount of money being payed in dollars
+	 */
+	public void Pay(double amount)
+	{
+		this.payedCents += Convert(amount);
+	}
+	
+	/**
+	 * Calculates and returns the change for the client's purchase
+	 * @return change in dollars
+	 */
+	public double recieveChange()
+	{
+		double cents = this.payedCents - this.cents;
+		if (cents < 0)
+		{
+			System.out.println("You are short $" + (cents / 100) * -1);
+			return 0;
+		} else
+		{
+			Reset();
+			return cents / 100;
+		}
+	}
+	
+	/**
+	 * Returns total number of purchased items
+	 * @return number of items
+	 */
+	public int getItemCount()
+	{
+		return this.itemCount;
+	}
+
+}
---
+++ Ap Comp Sci/chapter3/p3point1/P3point2.java
@@ -0,0 +1,63 @@
+package p3point1;
+
+public class P3point2
+{
+
+	public static void main(String[] args)
+	{
+		CashRegister cash = new CashRegister(.08);
+		System.out.println("Test 1 (no tax): ");
+		Test1(cash);
+		System.out.println("\nTest 2 (with tax): ");
+		Test2(cash);
+		System.out.println("\nTest 3 (not enough money payed, guy gives up): ");
+		Test3(cash);
+		System.out.println("\nTest 4 (not enough money payed, guy pays more): ");
+		Test4(cash);
+	}
+	
+	public static void Test1(CashRegister cash)
+	{
+		cash.PurchaseItem(12.54, false);
+		cash.PurchaseItem(13.98, false);
+		cash.PurchaseItem(0.45, false);
+		cash.PurchaseItem(15, false);
+		cash.Pay(50);
+		System.out.println("Item count: " + cash.getItemCount());
+		System.out.println(cash.recieveChange());
+	}
+	
+	public static void Test2(CashRegister cash)
+	{
+		cash.PurchaseItem(10, true);
+		cash.PurchaseItem(20, true);
+		cash.PurchaseItem(10, true);
+		cash.Pay(100);
+		System.out.println("Item count: " + cash.getItemCount());
+		System.out.println(cash.recieveChange());
+	}
+	
+	public static void Test3(CashRegister cash)
+	{
+		cash.PurchaseItem(87.45, false);
+		cash.PurchaseItem(23.90, false);
+		cash.PurchaseItem(12.45, true);
+		cash.PurchaseItem(34.56, true);
+		cash.Pay(50);
+		System.out.println(cash.recieveChange());
+		System.out.println(cash.Reset());
+	}
+	
+	public static void Test4(CashRegister cash)
+	{
+		cash.PurchaseItem(37.45, true);
+		cash.PurchaseItem(63.45, false);
+		cash.PurchaseItem(11.35, true);
+		cash.PurchaseItem(34.56, false);
+		cash.Pay(50);
+		System.out.println(cash.recieveChange());
+		cash.Pay(105);
+		System.out.println(cash.recieveChange());
+	}
+
+}
