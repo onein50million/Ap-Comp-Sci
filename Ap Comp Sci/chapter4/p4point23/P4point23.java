298c391b5934a99ff2643b09e0cd641728797dba
529565 <529565@8RM4-01.cuhsd-int.prv>
2015-10-02 11:34:58 -0700

    OCD

--- Ap Comp Sci/chapter4/p4point23/P4point23.java
+++ Ap Comp Sci/chapter4/p4point23/P4point23.java
@@ -8,12 +8,12 @@ public class P4point23
 		Balloon b = new Balloon();
 		b.addAir(100);
 		System.out.printf("The radius is: %.2f cm", b.getRadius());
-		System.out.printf("\nThe voulme is: %.2f cm³", b.getVoulme());
 		System.out.printf("\nThe surface area is: %.2f cm²", b.getSurfaceArea());
+		System.out.printf("\nThe voulme is: %.2f cm³", b.getVoulme());
 		b.addAir(100);
 		System.out.printf("\n\nThe radius is: %.2f cm", b.getRadius());
-		System.out.printf("\nThe voulme is: %.2f cm³", b.getVoulme());
 		System.out.printf("\nThe surface area is: %.2f cm²", b.getSurfaceArea());
+		System.out.printf("\nThe voulme is: %.2f cm³", b.getVoulme());
 	}
 
 }
