4d213af3f44bcbea472787a34de91a907ed66982
529565 <529565@8RM4-01.cuhsd-int.prv>
2015-10-06 11:59:26 -0700

    Improved testing, checking, and added doc comments

--- Ap Comp Sci/chapter4/p4point3/P4point3.java
+++ Ap Comp Sci/chapter4/p4point3/P4point3.java
@@ -5,11 +5,28 @@ public class P4point3
 
 	public static void main(String[] args)
 	{
+		System.out.println("Triangle 1");
+		Test1();
+		System.out.println("\n\nTriangle 2");
+		Test2();
+	}
+	
+	public static void Test1()
+	{
 		double[] lens = new double[3];
 		Triangle t = new Triangle();
 		t.addPoint(1, 2);
 		t.addPoint(6, 3);
 		t.addPoint(15, 7);
+		System.out.printf("Perimeter: %.2f", t.getPerimeter());
+		lens = t.getSideLengths();
+		System.out.printf("\nSide lengths: %.2f %.2f %.2f", lens[0], lens[1], lens[2]);
+	}
+	
+	public static void Test2()
+	{
+		double[] lens = new double[3];
+		Triangle t = new Triangle(2, 4, -4, 5, 10, 3);
 		System.out.printf("Perimeter: %.2f", t.getPerimeter());
 		lens = t.getSideLengths();
 		System.out.printf("\nSide lengths: %.2f %.2f %.2f", lens[0], lens[1], lens[2]);
--- Ap Comp Sci/chapter4/p4point3/Triangle.java
+++ Ap Comp Sci/chapter4/p4point3/Triangle.java
@@ -3,58 +3,97 @@ package p4point3;
 public class Triangle
 {
 	private double[] points = new double[6];
-	private double[] lengths = new double[3];
 	private int navigator = 0;
-
+	
+	/**
+	 * Initializes a triangle
+	 */
 	public Triangle()
 	{
 
 	}
-
+	
+	/**
+	 * Initializes a triangle with the given points
+	 * @param x1 x coordinate of the first point
+	 * @param y1 y coordinate of the first point
+	 * @param x2 x coordinate of the second point
+	 * @param y2 y coordinate of the second point
+	 * @param x3 x coordinate of the third point
+	 * @param y3 y coordinate of the third point
+	 */
 	public Triangle(double x1, double y1, double x2, double y2, double x3, double y3)
 	{
 		points[0] = x1;
-		points[1] = x2;
-		points[2] = x3;
-		points[3] = y1;
-		points[4] = y2;
+		points[1] = y1;
+		points[2] = x2;
+		points[3] = y2;
+		points[4] = x3;
 		points[5] = y3;
 		navigator = 6;
 	}
-
+	
+	/**
+	 * Returns the side lengths of the triangle
+	 * @return an array of the side lengths
+	 */
 	public double[] getSideLengths()
-	{
+	{	
 		double dx;
 		double dy;
 		double[] len = new double[3];
 		
-		dx = points[0] - points[2];
-		dy = points[1] - points[3];
-		len[0] = getLen(dx, dy);
-		
-		dx = points[2] - points[4];
-		dy = points[3] - points[5];
-		len[1] = getLen(dx, dy);
-		
-		dx = points[4] - points[0];
-		dy = points[5] - points[1];
-		len[2] = getLen(dx, dy);
-		
+		if (navigator != 6)
+		{
+			System.out.println("Not enough points");
+		}
+		else
+		{
+			dx = points[0] - points[2];
+			dy = points[1] - points[3];
+			len[0] = getLen(dx, dy);
+			
+			dx = points[2] - points[4];
+			dy = points[3] - points[5];
+			len[1] = getLen(dx, dy);
+			
+			dx = points[4] - points[0];
+			dy = points[5] - points[1];
+			len[2] = getLen(dx, dy);
+			
+		}
 		return len;
 	}
-
+	
+	/**
+	 * Returns the perimeter of the triangle
+	 * @return perimeter of the triangle
+	 */
 	public double getPerimeter()
 	{
 		double n = 0;
+		double[] lengths = new double[3];
 		
-		lengths = getSideLengths();
-		for (int i = 0; i < lengths.length; i++)
+		if (navigator != 6)
 		{
-			n += lengths[i];
+			System.out.println("Not enough points");
+		}
+		else
+		{	
+			lengths = getSideLengths();
+			for (int i = 0; i < lengths.length; i++)
+			{
+				n += lengths[i];
+			}
 		}
 		return n;
 	}
-
+	
+	/**
+	 * Adds a point to the triangle
+	 * @param x x coordinate of the point
+	 * @param y y coordinate of the point
+	 */
 	public void addPoint(double x, double y)
 	{
 		if (navigator != 6 && checkPoint(x, y))
@@ -62,13 +101,17 @@ public class Triangle
 			points[navigator] = x;
 			points[navigator + 1] = y;
 			navigator += 2;
-		} else
+		} 
+		else
 		{
 			System.out.printf("Discarded point: (%.0f, %.0f)", x, y);
 		}
 
 	}
-
+	
+	/**
+	 * Clears all points
+	 */
 	public void clearPoints()
 	{
 		for (int i = 0; i < points.length; i++)
