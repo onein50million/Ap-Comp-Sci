dbafbf77e51d7534f492d31cbf6bfecba7bc8361
529565 <529565@8RM4-01.cuhsd-int.prv>
2015-10-26 11:18:48 -0700

    if (true)

--- Ap Comp Sci/src/Tests.java
+++ Ap Comp Sci/src/Tests.java
@@ -3,9 +3,9 @@ public class Tests
 
 	public static void main(String[] args)
 	{
-		int x = 0;
-		int y = 0;
-		System.out.println(x == 0 ^ y == 0);
+		if (true)
+		{
+			System.out.println(true);
+		}
 	}
-
 }
