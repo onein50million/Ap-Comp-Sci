122b4aa5292a8fd3a6404cb5bd71242f46731f69
529565 <529565@8RM4-01.cuhsd-int.prv>
2015-11-03 11:35:41 -0800

    Made tests look stylish

--- Ap Comp Sci/src/Tests.java
+++ Ap Comp Sci/src/Tests.java
@@ -6,13 +6,14 @@ public class Tests
 	public static void main(String[] args)
 	{
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter things: ");
-		String i = "";
-		while (scan.hasNext())
-		{
-			i += scan.next();
-		}
-		System.out.println(i);
+		System.out.print("Enter a: ");
+		int a = scan.nextInt();
+		System.out.print("Enter b: ");
+		int b = scan.nextInt();
 		scan.close();
+		int sum = 0;
+		int sum2 = 0;
+		System.out.println();
+		for(int i=a%2==1?a:a+1;i<=b;System.out.println(i),i+=2);System.out.println();for(int i=a%2==0?a:a+1;i<=b;sum+=i,i+=2);System.out.println(sum+"\n");for(int i=1;i<=10;System.out.printf("%-2d %-3d\n",i,i*i),i++);System.out.println();for(int i=a%2==1?a:a+1;i<=b;sum2+=i*i,i+=2);System.out.println(sum2);
 	}
 }
