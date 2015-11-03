d123690a3de1cc14ef517f65dce436d978b32748
529565 <529565@8RM4-01.cuhsd-int.prv>
2015-11-03 11:25:03 -0800

    Added comments

--- Ap Comp Sci/chapter6/P6point1.java
+++ Ap Comp Sci/chapter6/P6point1.java
@@ -15,12 +15,20 @@ public class P6point1
 		int sum = 0;
 		int sum2 = 0;
 		System.out.println();
+		
+		//Outputs all the odd numbers between a and b inclusive
 		for (int i = a % 2 == 1 ? a : a + 1; i <= b; System.out.println(i), i += 2);
 		System.out.println();
+		
+		//Outputs the sum of all even numbers between a and b inclusive
 		for (int i = a % 2 == 0 ? a : a + 1; i <= b; sum += i, i += 2);
 		System.out.println(sum + "\n");
+		
+		//Outputs all the numbers and their squares between 1 and 10 inclusive
 		for (int i = 1; i <= 10; System.out.printf("%-2d %-3d\n", i, i * i), i++);
 		System.out.println();
+		
+		//Outputs the sum of the squares of all the odd numbers between a and b inclusive
 		for (int i = a % 2 == 1 ? a : a + 1; i <= b; sum2 += i * i, i += 2);
 		System.out.println(sum2);
 	}
