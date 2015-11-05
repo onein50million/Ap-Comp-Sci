64648127f84db8708b3d91550f43473bdcb121cf
529565 <529565@8RM4-01.cuhsd-int.prv>
2015-11-05 10:24:09 -0800

    I did work

--- Ap Comp Sci/chapter6/P6point1.java
+++ Ap Comp Sci/chapter6/P6point1.java
@@ -14,19 +14,19 @@ public class P6point1
 		scan.close();
 		int sum = 0;
 		int sum2 = 0;
-		System.out.println();
+		System.out.println("All odd numbers:");
 		
 		//Outputs all the odd numbers between a and b inclusive
-		for (int i = a % 2 == 1 ? a : a + 1; i <= b; System.out.println(i), i += 2);
-		System.out.println();
+		for (int i = a % 2 == 1 ? a : a + 1; i <= b; System.out.print(i + " "), i += 2);
+		System.out.println("\n\nSum of even numbers:");
 		
 		//Outputs the sum of all even numbers between a and b inclusive
 		for (int i = a % 2 == 0 ? a : a + 1; i <= b; sum += i, i += 2);
-		System.out.println(sum + "\n");
+		System.out.println(sum + "\n\nAll numbers between 1 and 10 & their squares:");
 		
 		//Outputs all the numbers and their squares between 1 and 10 inclusive
 		for (int i = 1; i <= 10; System.out.printf("%-2d %-3d\n", i, i * i), i++);
-		System.out.println();
+		System.out.println("\nSum of all odd number squares:");
 		
 		//Outputs the sum of the squares of all the odd numbers between a and b inclusive
 		for (int i = a % 2 == 1 ? a : a + 1; i <= b; sum2 += i * i, i += 2);
---
+++ Ap Comp Sci/chapter6/P6point2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+
+public class P6point2
+{
+
+	public static void main(String[] args)
+	{
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Input x: ");
+		int x = scan.nextInt();
+		System.out.print("Input y: ");
+		int y = scan.nextInt();
+		scan.close();
+		while (x != 0 && y != 0)
+		{
+			if (x > y)
+			{
+				x -= y;
+			}
+			else
+			{
+				y -= x;
+			}
+		}
+		System.out.println("The greatest common divisor is: " + (x == 0 ? y : x));
+	}
+	
+}
