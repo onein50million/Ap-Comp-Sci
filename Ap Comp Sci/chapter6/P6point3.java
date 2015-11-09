1174e2761f88758a05fe033e0455acd79802c2eb
529565 <529565@8RM4-01.cuhsd-int.prv>
2015-11-09 10:15:37 -0800

    Done

--- Ap Comp Sci/chapter6/P6point3.java
+++ Ap Comp Sci/chapter6/P6point3.java
@@ -4,7 +4,35 @@ public class P6point3
 
 	public static void main(String[] args)
 	{
-		
+		int n = 120;
+		int i;
+		while (!checkPrime(n))
+		{
+			i = 2;
+			while (true)
+			{
+				if (n % i == 0)
+				{
+					System.out.println(i);
+					n = n / i;
+					break;
+				}
+				i++;
+			}
+		}
+		System.out.println(n);
+	}
+	
+	public static boolean checkPrime(int n)
+	{
+		for (int i = 2; i <= Math.round(n / 2); i++)
+		{
+			if (n % i == 0)
+			{
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
