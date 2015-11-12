24a4b93ec064568cff07109862d07507469ec25b
529565 <529565@8RM4-01.cuhsd-int.prv>
2015-11-12 09:59:27 -0800

    Made faster

--- Ap Comp Sci/chapter6/P6point3.java
+++ Ap Comp Sci/chapter6/P6point3.java
@@ -12,6 +12,7 @@ public class P6point3
 				{
 					System.out.println(i);
 					n = n / i;
+					i = 1;
 				}
 			}
 			System.out.println(n);
@@ -19,17 +20,5 @@ public class P6point3
 		}
 	}
 
-	public static int checkPrime(int n)
-	{
-		for (int i = 2; i <= Math.round(n / 2); i++)
-		{
-			if (n % i == 0)
-			{
-				System.out.println(i);
-				return n / i;
-			}
-		}
-		return n;
-	}
 
 }
